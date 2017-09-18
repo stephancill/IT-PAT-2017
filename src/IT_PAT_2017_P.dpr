de0607dc333f8dd2a4f90da659dddbbcd2c3ad6e
Stephanus Cilliers <scilliers@parklands.co.za>
2017-09-18 12:04:11 +0200

    Added functionality to Register form, cleaned up Login form

--- src/IT_PAT_2017_P.dpr
+++ src/IT_PAT_2017_P.dpr
@@ -2,13 +2,15 @@ program IT_PAT_2017_P;
 
 uses
   Forms,
-  Login_U in 'Login_U.pas' {LoginForm};
+  Login_U in 'Login_U.pas' {frmLogin},
+  Register_U in 'Register_U.pas' {frmRegister};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TLoginForm, LoginForm);
+  Application.CreateForm(TfrmLogin, frmLogin);
+  Application.CreateForm(TfrmRegister, frmRegister);
   Application.Run;
 end.
